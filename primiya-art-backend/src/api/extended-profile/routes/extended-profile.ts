47dbd6eaec3aecb896d5393bb90f9a88cdaf4511
Parasuram T <tspsparasuram2004@gmail.com>
2025-11-10 19:39:27 +0530

    new

---
+++ primiya-art-backend/src/api/extended-profile/routes/extended-profile.ts
@@ -0,0 +1,7 @@
+/**
+ * extended-profile router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::extended-profile.extended-profile');
---
+++ primiya-art-backend/src/api/extended-profile/services/extended-profile.ts
@@ -0,0 +1,7 @@
+/**
+ * extended-profile service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::extended-profile.extended-profile');
